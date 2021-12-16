f91cdb26b5829e110907fba32c12b242f1848345
misabiko <misabiko@gmail.com>
2021-12-16 13:50:59 -0500

    Modal as function component

--- src/modals/mod.rs
+++ src/modals/mod.rs
@@ -4,10 +4,6 @@ pub mod add_timeline;
 
 pub use add_timeline::AddTimelineModal;
 
-pub struct Modal;
-
-pub enum Msg {}
-
 #[derive(Properties, PartialEq, Clone)]
 pub struct Props {
 	pub modal_title: String,
@@ -17,42 +13,30 @@ pub struct Props {
 	pub footer: Html,
 }
 
-impl Component for Modal {
-	type Message = Msg;
-	type Properties = Props;
-
-	fn create(_ctx: &Context<Self>) -> Self {
-		Self {}
-	}
-
-	fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-		false
-	}
-
-	fn view(&self, ctx: &Context<Self>) -> Html {
-		html! {
-			<div class="modal is-active">
-				<div class="modal-background"/>
-				<div class="modal-content">
-					<div class="card">
-						<header class="card-header">
-							<p class="card-header-title">{"Choose Endpoint"}</p>
-							<button class="card-header-icon" onclick={ctx.props().close_modal_callback.clone()}>
-								<span class="icon">
-									<i class="fas fa-times"/>
-								</span>
-							</button>
-						</header>
-						<div class="card-content">
-							{ for ctx.props().children.iter() }
-						</div>
-						<footer class="card-footer">
-							{ ctx.props().footer.clone() }
-						</footer>
+#[function_component(Modal)]
+pub fn modal(props: &Props) -> Html {
+	html! {
+		<div class="modal is-active">
+			<div class="modal-background"/>
+			<div class="modal-content">
+				<div class="card">
+					<header class="card-header">
+						<p class="card-header-title">{"Choose Endpoint"}</p>
+						<button class="card-header-icon" onclick={props.close_modal_callback.clone()}>
+							<span class="icon">
+								<i class="fas fa-times"/>
+							</span>
+						</button>
+					</header>
+					<div class="card-content">
+						{ for props.children.iter() }
 					</div>
+					<footer class="card-footer">
+						{ props.footer.clone() }
+					</footer>
 				</div>
 			</div>
-		}
+		</div>
 	}
 }
 
