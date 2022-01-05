3ee6445285d07ae85f674394072ef6180eb5f0d6
misabiko <misabiko@gmail.com>
2022-01-05 09:23:47 -0500

    Moved main TODOs to github

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,3 @@ fn main() {
 		Some(Err(err)) => log::error!("Failed to get location.href.\n{}", &err.as_string().unwrap_or("Failed to parse the error.".to_string())),
 	};
 }
-
-//TODO Profile lag when redrawing articles
-//TODO Youtube articles
-//TODO Have custom service setting view
-//TODO Show quato units for Youtube service
-//TODO Cache playlist id for each subscribed channel
-//TODO Custom social buttons per article type
-//TODO Notifications
-//TODO Save timeline data
-//TODO Display timeline errors
-//TODO Prompt on not logged in
-//TODO Avoid refreshing endpoint every watch update
-//TODO Add "Open @myusername on soshalthing" context menu?
-
-//TODO Show multiple article types in same timeline
