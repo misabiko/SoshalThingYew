26b0c6c3bd56eebe3f577dc54dbda315ed78609e
misabiko <misabiko@gmail.com>
2022-02-11 22:04:27 -0500

    Added test for order when removing main timeline

--- tests/soshalthing.spec.js
+++ tests/soshalthing.spec.js
@@ -7,13 +7,8 @@ test('visits the app root url', async ({page}) => {
 test('timeline without endpoint', async ({page}) => {
 	await page.goto('/');
 	await page.mainFrame().evaluate(() => {
-		window.localStorage.setItem('SoshalThingYew', JSON.stringify({
-			display_mode: {
-				type: "Single",
-			}
-		}));
 		window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
-			{"title": "Home"}
+			{"title": "Timeline"}
 		]));
 	});
 	await page.reload();
@@ -25,35 +20,60 @@ test('timeline without endpoint', async ({page}) => {
 	await expect(page.locator("#sidebar > .sidebarMenu > div.box").nth(2)).toBeEmpty();
 });
 
-test('main timeline search param', async ({page}) => {
-	await page.goto('/');
-	await page.mainFrame().evaluate(() => {
-		window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
-			{"title": "Home"}
-		]));
+test.describe('main timeline', () => {
+	test('via search param', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Timeline"}
+			]));
+		});
+		await page.goto('/?single_timeline=true');
+
+		await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
 	});
-	await page.goto('/?single_timeline=true');
 
-	await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
-});
+	test('via local storage', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew', JSON.stringify({
+				display_mode: {
+					type: "Single",
+					container: "Column",
+					column_count: 1,
+				}
+			}));
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Timeline"}
+			]));
+		});
+		await page.reload();
 
-test('main timeline storage', async ({page}) => {
-	await page.goto('/');
-	await page.mainFrame().evaluate(() => {
-		window.localStorage.setItem('SoshalThingYew', JSON.stringify({
-			display_mode: {
-				type: "Single",
-				container: "Column",
-				column_count: 1,
-			}
-		}));
-		window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
-			{"title": "Home"}
-		]));
+		await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
 	});
-	await page.reload();
 
-	await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
+	test('removing main timeline retains order', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Timeline1"},
+				{"title": "Timeline2"},
+				{"title": "Timeline3"},
+			]));
+		});
+		await page.reload();
+
+		await page.click('.timeline:nth-child(2) .timelineHeader .timelineButtons button[title = "Expand options"]');
+
+		await page.click('button:has-text("Set as main timeline")');
+
+		await page.click('text=Remove timeline');
+
+		await page.click('#sidebarButtons button[title = "Multiple Timeline"]');
+
+		await expect(page.locator('.timeline:first-child .timelineLeftHeader > strong')).toHaveText('Timeline1');
+		await expect(page.locator('.timeline:nth-child(2) .timelineLeftHeader > strong')).toHaveText('Timeline3');
+	});
 });
 
 test.skip('repost feedback', async ({page}) => {
@@ -67,7 +87,7 @@ test.skip('repost feedback', async ({page}) => {
 			}
 		}));
 		window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([{
-			"title": "Home",
+			"title": "Timeline",
 			"endpoints": [
 				{
 					"service": "Dummy Service",
