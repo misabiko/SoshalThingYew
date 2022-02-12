7d9c5783139a71d3515706375e8808c5b31db1b2
misabiko <misabiko@gmail.com>
2022-02-11 21:35:27 -0500

    Set localstorage without test.use

--- tests/spec.js
+++ tests/spec.js
@@ -5,136 +5,81 @@ test.describe('SoshalThing', () => {
 		await page.goto('/');
 	});
 
-	test.describe('', () => {
-		test.use({
-			storageState: {
-				origins: [{
-					origin: 'http://localhost:8080',
-					localStorage: [
-						{
-							name: 'SoshalThingYew',
-							value: JSON.stringify({
-								display_mode: {
-									type: "Single",
-								}
-							})
-						},
-						{
-							name: 'SoshalThingYew Timelines',
-							value: JSON.stringify([
-								{"title": "Home"}
-							])
-						}
-					]
-				}]
-			}
+	test('timeline without endpoint', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew', JSON.stringify({
+				display_mode: {
+					type: "Single",
+				}
+			}));
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Home"}
+			]));
 		});
-		test('timeline without endpoint', async ({page}) => {
-			await page.goto('/');
+		await page.reload();
 
-			await expect(page.locator('.timeline')).toHaveCount(1);
+		await expect(page.locator('.timeline')).toHaveCount(1);
 
-			await page.click("#sidebarButtons button[title = 'Expand sidebar']");
+		await page.click("#sidebarButtons button[title = 'Expand sidebar']");
 
-			await expect(page.locator("#sidebar > .sidebarMenu > div.box").nth(2)).toBeEmpty();
-		});
+		await expect(page.locator("#sidebar > .sidebarMenu > div.box").nth(2)).toBeEmpty();
 	});
 
-	test.describe('', () => {
-		test.use({
-			storageState: {
-				origins: [{
-					origin: 'http://localhost:8080',
-					localStorage: [
-						{
-							name: 'SoshalThingYew Timelines',
-							value: JSON.stringify([
-								{"title": "Home"}
-							])
-						}
-					]
-				}]
-			}
+	test('main timeline search param', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Home"}
+			]));
 		});
-		test('main timeline search param', async ({page}) => {
-			await page.goto('/?single_timeline=true');
+		await page.goto('/?single_timeline=true');
 
-			await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
-		});
+		await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
 	});
 
-	test.describe('', () => {
-		test.use({
-			storageState: {
-				origins: [{
-					origin: 'http://localhost:8080',
-					localStorage: [
-						{
-							name: 'SoshalThingYew',
-							value: JSON.stringify({
-								display_mode: {
-									type: "Single",
-									container: "Column",
-									column_count: 1,
-								}
-							})
-						},
-						{
-							name: 'SoshalThingYew Timelines',
-							value: JSON.stringify([
-								{"title": "Home"}
-							])
-						}
-					]
-				}]
-			}
+	test('main timeline storage', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew', JSON.stringify({
+				display_mode: {
+					type: "Single",
+					container: "Column",
+					column_count: 1,
+				}
+			}));
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([
+				{"title": "Home"}
+			]));
 		});
-		test('main timeline storage', async ({page}) => {
-			await page.goto('/');
+		await page.reload();
 
-			await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
-		});
+		await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
 	});
 
-	test.describe('', () => {
-		test.use({
-			storageState: {
-				origins: [{
-					origin: 'http://localhost:8080',
-					localStorage: [
-						{
-							name: 'SoshalThingYew',
-							value: JSON.stringify({
-								display_mode: {
-									type: "Single",
-									container: "Column",
-									column_count: 1,
-								}
-							})
-						},
-						{
-							name: 'SoshalThingYew Timelines',
-							value: JSON.stringify([
-								{
-									"title": "Home",
-									"endpoints": [
-										{
-											"service": "Dummy Service",
-											"endpoint_type": 0,
-											"params": {},
-											"on_start": true,
-											"on_refresh": true
-										}
-									]
-								}
-							])
-						}
-					]
-				}]
-			}
+	test.skip('repost feedback', async ({page}) => {
+		await page.goto('/');
+		await page.mainFrame().evaluate(() => {
+			window.localStorage.setItem('SoshalThingYew', JSON.stringify({
+				display_mode: {
+					type: "Single",
+					container: "Column",
+					column_count: 1,
+				}
+			}));
+			window.localStorage.setItem('SoshalThingYew Timelines', JSON.stringify([{
+				"title": "Home",
+				"endpoints": [
+					{
+						"service": "Dummy Service",
+						"endpoint_type": 0,
+						"params": {},
+						"on_start": true,
+						"on_refresh": true
+					}
+				]
+			}]));
 		});
-		test.skip('repost feedback', async ({page}) => {
-			await page.goto('/');
-		});
+		await page.reload();
 	});
 });
