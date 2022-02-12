2bd02210e4e410da14cbe28b02b83f07fbd5a1b1
misabiko <misabiko@gmail.com>
2022-02-11 19:41:25 -0500

    Ported rest of tests to Playwright

--- tests/spec.js
+++ tests/spec.js
@@ -1,82 +1,140 @@
-const { expect, test } = require('@playwright/test');
+const {expect, test} = require('@playwright/test');
 
 test.describe('SoshalThing', () => {
-	test('visits the app root url', async ({ page }) => {
+	test('visits the app root url', async ({page}) => {
 		await page.goto('/');
-	})
+	});
 
-	/*it('timeline without endpoint', () => {
-		cy.session("timeline without endpoint", () => {
-			window.localStorage.setItem("SoshalThingYew", JSON.stringify({
-				display_mode: {
-					type: "Single",
-				}
-			}));
-			window.localStorage.setItem("SoshalThingYew Timelines", JSON.stringify([
-				{
-					"title": "Home"
-				}
-			]));
-		});
-		cy.visit('/');
-
-		cy.get(".timeline")
-
-		cy.get("#sidebarButtons button[title = 'Expand sidebar']").click()
-
-		cy.get("#sidebar > .sidebarMenu > div.box").eq(2).should("be.empty")
-	})
-
-	it('main timeline search param', () => {
-		cy.session("main timeline search param", () => {
-			window.localStorage.setItem("SoshalThingYew Timelines", JSON.stringify([
-				{
-					"title": "Home"
-				}
-			]));
-		});
-		cy.visit('/?single_timeline=true');
-
-		cy.get(".timeline.mainTimeline")
-	})
-
-	it('main timeline storage', () => {
-		cy.session("main timeline storage", () => {
-			window.localStorage.setItem("SoshalThingYew", JSON.stringify({
-				display_mode: {
-					type: "Single",
-					container: "Column",
-					column_count: 1,
-				}
-			}));
-			window.localStorage.setItem("SoshalThingYew Timelines", JSON.stringify([
-				{
-					"title": "Home"
-				}
-			]));
-		});
-		cy.visit('/');
-
-		cy.get(".timeline.mainTimeline")
-	})
-
-	it.skip('repost feedback', () => {
-		cy.session("repost feedback", () => {
-			window.localStorage.setItem("SoshalThingYew Timelines", JSON.stringify([
-				{
-					"title": "Home",
-					"endpoints": [
+	test.describe('', () => {
+		test.use({
+			storageState: {
+				origins: [{
+					origin: 'http://localhost:8080',
+					localStorage: [
 						{
-							"service": "Dummy Service",
-							"endpoint_type": 0,
-							"params": {},
-							"on_start": true,
-							"on_refresh": true
+							name: 'SoshalThingYew',
+							value: JSON.stringify({
+								display_mode: {
+									type: "Single",
+								}
+							})
+						},
+						{
+							name: 'SoshalThingYew Timelines',
+							value: JSON.stringify([
+								{"title": "Home"}
+							])
 						}
 					]
-				}
-			]));
+				}]
+			}
 		});
-		cy.visit('/');
-	})*/
-})
+		test('timeline without endpoint', async ({page}) => {
+			await page.goto('/');
+
+			await expect(page.locator('.timeline')).toHaveCount(1);
+
+			await page.click("#sidebarButtons button[title = 'Expand sidebar']");
+
+			await expect(page.locator("#sidebar > .sidebarMenu > div.box").nth(2)).toBeEmpty();
+		});
+	});
+
+	test.describe('', () => {
+		test.use({
+			storageState: {
+				origins: [{
+					origin: 'http://localhost:8080',
+					localStorage: [
+						{
+							name: 'SoshalThingYew Timelines',
+							value: JSON.stringify([
+								{"title": "Home"}
+							])
+						}
+					]
+				}]
+			}
+		});
+		test('main timeline search param', async ({page}) => {
+			await page.goto('/?single_timeline=true');
+
+			await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
+		});
+	});
+
+	test.describe('', () => {
+		test.use({
+			storageState: {
+				origins: [{
+					origin: 'http://localhost:8080',
+					localStorage: [
+						{
+							name: 'SoshalThingYew',
+							value: JSON.stringify({
+								display_mode: {
+									type: "Single",
+									container: "Column",
+									column_count: 1,
+								}
+							})
+						},
+						{
+							name: 'SoshalThingYew Timelines',
+							value: JSON.stringify([
+								{"title": "Home"}
+							])
+						}
+					]
+				}]
+			}
+		});
+		test('main timeline storage', async ({page}) => {
+			await page.goto('/');
+
+			await expect(page.locator('.timeline').first()).toHaveClass(/mainTimeline/);
+		});
+	});
+
+	test.describe('', () => {
+		test.use({
+			storageState: {
+				origins: [{
+					origin: 'http://localhost:8080',
+					localStorage: [
+						{
+							name: 'SoshalThingYew',
+							value: JSON.stringify({
+								display_mode: {
+									type: "Single",
+									container: "Column",
+									column_count: 1,
+								}
+							})
+						},
+						{
+							name: 'SoshalThingYew Timelines',
+							value: JSON.stringify([
+								{
+									"title": "Home",
+									"endpoints": [
+										{
+											"service": "Dummy Service",
+											"endpoint_type": 0,
+											"params": {},
+											"on_start": true,
+											"on_refresh": true
+										}
+									]
+								}
+							])
+						}
+					]
+				}]
+			}
+		});
+		test.skip('repost feedback', async ({page}) => {
+			await page.goto('/');
+		});
+	});
+});
